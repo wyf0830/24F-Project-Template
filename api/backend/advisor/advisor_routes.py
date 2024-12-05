f52f9619e865e8fb74120f1e46e836cdb5a86c35
Al13n0231 <139857884+Al13n0231@users.noreply.github.com>
2024-12-05 17:20:30 -0500

    working on API problem with advisor_routes.py

--- api/backend/advisor/advisor_routes.py
+++ api/backend/advisor/advisor_routes.py
@@ -2,82 +2,61 @@
 # Sample customers blueprint of endpoints
 # Remove this file if you are not using it in your project
 ########################################################
-from flask import Blueprint
-from flask import request
-from flask import jsonify
-from flask import make_response
-from flask import current_app
+from flask import Blueprint, request, jsonify, make_response
 from backend.db_connection import db
-from backend.ml_models.model01 import predict
 
-#------------------------------------------------------------
-# Create a new Blueprint object, which is a collection of 
-# routes.
-customers = Blueprint('customers', __name__)
+# Create a Blueprint for Advisor routes
+advisor = Blueprint('advisor', __name__)
 
-
-#------------------------------------------------------------
-# Get all customers from the system
-@customers.route('/customers', methods=['GET'])
-def get_customers():
-
+# Get all advisors
+@advisor.route('/advisors', methods=['GET'])
+def get_advisors():
     cursor = db.get_db().cursor()
-    cursor.execute('''SELECT id, company, last_name,
-                    first_name, job_title, business_phone FROM customers
-    ''')
-    
-    theData = cursor.fetchall()
-    
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
-    return the_response
+    cursor.execute("SELECT * FROM Advisor")  # Replace with your actual Advisor table
+    advisors = cursor.fetchall()
+    return make_response(jsonify(advisors), 200)
 
-#------------------------------------------------------------
-# Update customer info for customer with particular userID
-#   Notice the manner of constructing the query.
-@customers.route('/customers', methods=['PUT'])
-def update_customer():
-    current_app.logger.info('PUT /customers route')
-    cust_info = request.json
-    cust_id = cust_info['id']
-    first = cust_info['first_name']
-    last = cust_info['last_name']
-    company = cust_info['company']
-
-    query = 'UPDATE customers SET first_name = %s, last_name = %s, company = %s where id = %s'
-    data = (first, last, company, cust_id)
+# Get an advisor by ID
+@advisor.route('/advisors/<int:advisor_id>', methods=['GET'])
+def get_advisor(advisor_id):
     cursor = db.get_db().cursor()
-    r = cursor.execute(query, data)
+    cursor.execute("SELECT * FROM Advisor WHERE Advisor_ID = %s", (advisor_id,))
+    advisor = cursor.fetchone()
+    if advisor:
+        return make_response(jsonify(advisor), 200)
+    else:
+        return make_response(jsonify({"error": "Advisor not found"}), 404)
+
+# Add a new advisor
+@advisor.route('/advisors', methods=['POST'])
+def add_advisor():
+    advisor_data = request.json
+    query = "INSERT INTO Advisor (Name, Email, Department) VALUES (%s, %s, %s)"
+    data = (advisor_data['name'], advisor_data['email'], advisor_data['department'])
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
     db.get_db().commit()
-    return 'customer updated!'
+    return make_response(jsonify({"message": "Advisor added successfully!"}), 201)
 
-#------------------------------------------------------------
-# Get customer detail for customer with particular userID
-#   Notice the manner of constructing the query. 
-@customers.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
-    current_app.logger.info('GET /customers/<userID> route')
+# Update an advisor
+@advisor.route('/advisors/<int:advisor_id>', methods=['PUT'])
+def update_advisor(advisor_id):
+    advisor_data = request.json
+    query = """
+    UPDATE Advisor
+    SET Name = %s, Email = %s, Department = %s
+    WHERE Advisor_ID = %s
+    """
+    data = (advisor_data['name'], advisor_data['email'], advisor_data['department'], advisor_id)
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT id, first_name, last_name FROM customers WHERE id = {0}'.format(userID))
-    
-    theData = cursor.fetchall()
-    
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
-    return the_response
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return make_response(jsonify({"message": "Advisor updated successfully!"}), 200)
 
-#------------------------------------------------------------
-# Makes use of the very simple ML model in to predict a value
-# and returns it to the user
-@customers.route('/prediction/<var01>/<var02>', methods=['GET'])
-def predict_value(var01, var02):
-    current_app.logger.info(f'var01 = {var01}')
-    current_app.logger.info(f'var02 = {var02}')
-
-    returnVal = predict(var01, var02)
-    return_dict = {'result': returnVal}
-
-    the_response = make_response(jsonify(return_dict))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+# Delete an advisor
+@advisor.route('/advisors/<int:advisor_id>', methods=['DELETE'])
+def delete_advisor(advisor_id):
+    cursor = db.get_db().cursor()
+    cursor.execute("DELETE FROM Advisor WHERE Advisor_ID = %s", (advisor_id,))
+    db.get_db().commit()
+    return make_response(jsonify({"message": "Advisor deleted successfully!"}), 200)
