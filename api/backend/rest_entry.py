60fa926f4ca2febdf2e0d0c65029069c58c43365
orim177 <yimingy17@gmail.com>
2024-12-05 16:32:33 -0500

    add rest_entry

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
 from backend.db_connection import db
+from backend.advisor.advisor_routes import advisor
+from backend.students.students_route import students
+from backend.director.director_route import director
+from backend.admin.admin_route import admin
 from backend.customers.customer_routes import customers
 from backend.products.products_routes import products
 from backend.simple.simple_routes import simple_routes
@@ -40,9 +44,10 @@ def create_app():
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(advisor,     url_prefix='/ad')
+    app.register_blueprint(admin,       url_prefix='/a')
     app.register_blueprint(director,    url_prefix='/d')
+    app.register_blueprint(students,    url_prefix='/s')
 
     # Don't forget to return the app object
     return app
